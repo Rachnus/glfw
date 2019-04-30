063fd93633e7ff448b52b88b02b1fc6bb8c2e3b4
Jimi <jimi.97@hotmail.com>
2019-04-30 14:42:34 +0300

    Updated premake and gitignore files

--- premake5.lua
+++ premake5.lua
@@ -28,12 +28,12 @@ project "GLFW"
             "src/win32_joystick.c",
             "src/win32_monitor.c",
             "src/win32_time.c",
-            "src/win32_tls.c",
-           -- "src/win32_thread.c",
+            -- "src/win32_tls.c",
+            "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
             "src/egl_context.c",
-          --  "src/osmesa_context.c"
+            "src/osmesa_context.c"
         }
 
         defines 
